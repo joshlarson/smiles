e3a1041d7b067e6cc9b1ace1789bf213c260712d
Josh Larson <theMutatedShrimp@gmail.com>
2016-03-01 19:38:34 -0600

    Add a top hat

--- main.rb
+++ main.rb
@@ -26,18 +26,29 @@ def transform(transformation, shapes)
   end
 end
 
-hat = [{:x => 0, :y => 100}, {:x => 150, :y => 150}, {:x => 100, :y => 0}]
+hat = [
+  {:x => -30, :y => 120},
+  {:x => -20, :y => 150},
+
+  {:x => 7, :y => 141},
+  {:x => 17, :y => 171},
+  {:x => 98, :y => 144},
+  {:x => 88, :y => 114},
+
+  {:x => 115, :y => 105},
+  {:x => 105, :y => 75},
+]
 face = create_circle(0, 0, 125)
 left_eye = create_circle(-50, 50, 20)
 right_eye = create_circle(50, 50, 20)
 mouth = create_arc(0, 0, 100, 0.55, 0.95)
 
 shapes = [
-  Shape.new(hat, Color.new(0, 0, 0), Color.new(255, 0, 0)),
   Shape.new(face, Color.new(0, 0, 0), Color.new(255, 255, 0)),
   Shape.new(left_eye, Color.new(0, 0, 0), Color.new(0, 0, 0)),
   Shape.new(right_eye, Color.new(0, 0, 0), Color.new(0, 0, 0)),
   Shape.new(mouth, Color.new(0, 0, 0), Color.new(0, 0, 0)),
+  Shape.new(hat, Color.new(0, 0, 0), Color.new(0, 0, 0)),
 ]
 
 a = ARGV[0].to_f
